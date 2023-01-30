aaff6183bc04e3015fa382d90e5ea762d9aa6264
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-01-30 11:59:29 +0530

    Update bank.java

--- src/main/java/je/bank.java
+++ src/main/java/je/bank.java
@@ -1,6 +1,7 @@
 package je;
 
 import java.util.*;
+import java.util.logging.Logger;
 class BANK{
     String accountHolderName;
     int accountNumber;
@@ -8,42 +9,43 @@ class BANK{
     static Scanner so = new Scanner(System.in);
 
 
-    bank(){
-        System.out.println("Bank Account");
-        System.out.println("Enter Your Name:");
+    BANK(){
+        Log.info("Bank Account");
+        Log.info("Enter Your Name:");
         accountHolderName = so.nextLine();
-        System.out.println("Enter Account number:");
+        Log.info("Enter Account number:");
         accountNumber = so.nextInt();
-        System.out.println("Account Created");
+        Log.info("Account Created");
     
     }
     void display(){
-        System.out.println("Account Holder Name: "+accountHolderName);
-        System.out.println("Account Number: "+accountNumber);
-        System.out.println("Balance: Rs. "+accountBalance);
+        Log.info("Account Holder Name: "+accountHolderName);
+        Log.info("Account Number: "+accountNumber);
+        Log.info("Balance: Rs. "+accountBalance);
     }
     void deposit(){
-        System.out.println("Enter Amount to deposit: Rs. ");
+        Log.info("Enter Amount to deposit: Rs. ");
         double depositAmt = so.nextDouble();
         accountBalance+=depositAmt;
-        System.out.println("Rs. "+depositAmt+" Deposited Successfully");
+        Log.info("Rs. "+depositAmt+" Deposited Successfully");
     }
     void withdraw(){
-        System.out.println("Enter Withdrawn amt: Rs. ");
+        Log.info("Enter Withdrawn amt: Rs. ");
         int withdrawAmt = so.nextInt();
         if(accountBalance>withdrawAmt){
             accountBalance-=withdrawAmt;
-            System.out.println("---Amount Withdrawn Succsessfully---\n Account Balance: Rs. "+accountBalance);
+            Log.info("---Amount Withdrawn Succsessfully---\n Account Balance: Rs. "+accountBalance);
         }
         else{
-            System.out.println("***Insufficient Amount***");
+            Log.info("***Insufficient Amount***");
         }
     }
+    public static final Logger Log = Logger.getLogger("InfoLogging");
  public static void main(String[] args) {
-    bank bk = new bank();
+    BANK bk = new BANK();
     int ch;
     while(true){
-    System.out.println("Enter Your choice \n 1.deposit\n 2.Withdraw\n 3.View Balance\n 4.exit");
+    Log.info("Enter Your choice \n 1.deposit\n 2.Withdraw\n 3.View Balance\n 4.exit");
     ch = so.nextInt();
     switch(ch){
         case 1: bk.deposit();break;
