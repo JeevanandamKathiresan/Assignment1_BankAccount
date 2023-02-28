5dbaf318248ea0ce089069a24f0ba498e4a5eab6
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-02-28 10:07:49 +0530

    Update BankAcc.java

--- src/main/java/bankaccount/BankAcc.java
+++ src/main/java/bankaccount/BankAcc.java
@@ -1,45 +1,31 @@
 package bankaccount;
-
 import java.util.*;
 import java.util.logging.Logger;
-class BankAcc{
+public class Bank{
     public static final Logger Log = Logger.getLogger("InfoLogging");
 
-    String accountHolderName;
-    int accountNumber;
-    double accountBalance=0;
-    static Scanner so = new Scanner(System.in);
+    public static void main(String[] args) {
+    BankAcc bk = new BankAcc();
+    int ch;
+    Scanner so = new Scanner(System.in);
+    while(true){
+    try{
 
-
-    BankAcc(){
-        Log.info("Bank Account");
-        Log.info("Enter Your Name:");
-        accountHolderName = so.nextLine();
-        Log.info("Enter Account number:");
-        accountNumber = so.nextInt();
-        Log.info("Account Created");
-    
-    }
-    void display(){
-        Log.info("Account Holder Name: "+accountHolderName);
-        Log.info("Account Number: "+accountNumber);
-        Log.info("Balance: Rs."+accountBalance);
-    }
-    void deposit(){
-        Log.info("Enter Amount to deposit: Rs. ");
-        double depositAmt = so.nextDouble();
-        accountBalance+=depositAmt;
-        Log.info("Rs. "+depositAmt+" Deposited Successfully");
-    }
-    void withdraw(){
-        Log.info("Enter Withdrawn amt: Rs. ");
-        int withdrawAmt = so.nextInt();
-        if(accountBalance>withdrawAmt){
-            accountBalance-=withdrawAmt;
-            Log.info("---Amount Withdrawn Succsessfully---\n Account Balance: Rs. "+accountBalance);
+    Log.info("Enter Your choice \n 1.deposit\n 2.Withdraw\n 3.View Balance\n 4.exit");
+    ch = so.nextInt();
+    switch(ch){
+        case 1: bk.deposit();break;
+        case 2: bk.withdraw();break;
+        case 3: bk.display();break;
+        case 4: System.exit(0);break;
+        default: System.exit(0); break;     
         }
-        else{
-            Log.warning("***Insufficient Amount***");
-        }
+       }
+       catch(InputMismatchException e){
+        String exp = "Exception:"+ e;
+        Log.info(exp);
+        so.nextLine();
     }
+    }  
+}
 }
