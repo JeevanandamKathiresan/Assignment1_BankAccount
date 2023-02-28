a07e98f7f189e738791872c4556a2f86d7f64ca8
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-02-28 10:14:53 +0530

    Update BankAcc.java

--- src/main/java/bankaccount/BankAcc.java
+++ src/main/java/bankaccount/BankAcc.java
@@ -1,31 +1,46 @@
 package bankaccount;
+
 import java.util.*;
 import java.util.logging.Logger;
-public class Bank{
+class BankAcc{
     public static final Logger Log = Logger.getLogger("InfoLogging");
 
-    public static void main(String[] args) {
-    BankAcc bk = new BankAcc();
-    int ch;
-    Scanner so = new Scanner(System.in);
-    while(true){
-    try{
+    String accountHolderName;
+    int accountNumber;
+    double accountBalance=0;
+    static Scanner so = new Scanner(System.in);
 
-    Log.info("Enter Your choice \n 1.deposit\n 2.Withdraw\n 3.View Balance\n 4.exit");
-    ch = so.nextInt();
-    switch(ch){
-        case 1: bk.deposit();break;
-        case 2: bk.withdraw();break;
-        case 3: bk.display();break;
-        case 4: System.exit(0);break;
-        default: System.exit(0); break;     
-        }
-       }
-       catch(InputMismatchException e){
-        String exp = "Exception:"+ e;
-        Log.info(exp);
-        so.nextLine();
+
+    BankAcc(){
+        Log.info("Bank Account");
+        Log.info("Enter Your Name:");
+        accountHolderName = so.nextLine();
+        Log.info("Enter Account number:");
+        accountNumber = so.nextInt();
+        Log.info("Account Created");
+    
     }
-    }  
-}
+    void display(){
+        String dis = "Account Holder Name: "+accountHolderName+"\nAccount Number: "+accountNumber+"\nBalance: Rs."+accountBalance;
+        Log.info(dis);
+        
+    }
+    void deposit(){
+        Log.info("Enter Amount to deposit: Rs. ");
+        double depositAmt = so.nextDouble();
+        accountBalance+=depositAmt;
+        Log.info(" Deposited Successfully");
+    }
+    void withdraw(){
+        Log.info("Enter Withdrawn amt: Rs. ");
+        int withdrawAmt = so.nextInt();
+        if(accountBalance>withdrawAmt){
+            accountBalance-=withdrawAmt;
+            String wd = "---Amount Withdrawn Succsessfully---\n Account Balance: Rs. "+accountBalance;
+            Log.info(wd);
+        }
+        else{
+            Log.warning("***Insufficient Amount***");
+        }
+    }
 }
